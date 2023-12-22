f2c474effca86ba7808b9b8d0015d99821041fee
drshtaant <adarshh.addi@gmail.com>
2023-12-22 11:00:20 +0530

    fix: image error fixed

--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import blackBackground from "@/assets/blackBackground.jpeg"
 
 function Blogs() {
   return (
@@ -10,12 +12,12 @@ function Blogs() {
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Pitchfork Kickstarter Taxidermy</h1>
         <div className="h-1 w-20 bg-indigo-500 rounded"></div>
       </div>
-      <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably have heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"/>
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src={blackBackground} alt="content"/>
           <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
           <h2 className="text-lg text-white font-medium title-font mb-4">Chichen Itza</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
@@ -23,7 +25,7 @@ function Blogs() {
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src={blackBackground} alt="content"/>
           <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
           <h2 className="text-lg text-white font-medium title-font mb-4">Colosseum Roma</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
@@ -31,7 +33,7 @@ function Blogs() {
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src={blackBackground} alt="content"/>
           <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
           <h2 className="text-lg text-white font-medium title-font mb-4">Great Pyramid of Giza</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
@@ -39,7 +41,7 @@ function Blogs() {
       </div>
       <div className="xl:w-1/4 md:w-1/2 p-4">
         <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
-          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
+          <Image className="h-40 rounded w-full object-cover object-center mb-6" src={blackBackground} alt="content"/>
           <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
           <h2 className="text-lg text-white font-medium title-font mb-4">San Francisco</h2>
           <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
