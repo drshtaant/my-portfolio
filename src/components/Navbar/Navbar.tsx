a537eece8569ac4c1ea97417dd317c6628b6e681
“drshtaant” <adarshh.addi@gmail.com>
2024-01-04 22:43:42 +0530

    feat: logo updated

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import logo from "@/assets/logo.jpeg"
+import Image from 'next/image'
 
 function Navbar() {
   return (
     <header className="text-gray-400 bg-gray-800 body-font">
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg> */}
+      <Image className="w-10 h-10 text-white rounded-full" src={logo} alt="Logo" height={18} width={40}/> 
       <span className="ml-3 text-2xl">Adarsh</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
