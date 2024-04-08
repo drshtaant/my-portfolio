b54544311fa2f72a3487bfa39a85e751897b5f64
“adarsh.kumar” <adarsh.kumar@softway.com>
2024-04-08 20:40:39 +0530

    feat: contact page email sending updated

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,60 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react';
 import heroData from "@/assets/Data.json";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    formData.append("access_key", "30ad49ab-555c-4c43-82cf-7be199a248ef");
+
+    const object: any = {};
+    formData.forEach((value, key) => {
+      object[key] = value;
+    });
+
+    const json = JSON.stringify(object);
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: json
+      });
+      
+      const result = await response.json();
+      
+      if (result.success) {
+        console.log(result);
+        setSubmitSuccess(true);
+        setFormData({
+          name: '',
+          email: '',
+          message: ''
+        });
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  }
 
   const contact = heroData.hero;
+  
   return (
-<section className="text-gray-400 bg-gray-800 body-font relative">
-  <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-    <div className="lg:w-2/3 md:w-1/2 bg-gray-800 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-  <iframe
-  width="100%"
-  height="100%"
-  title="map"
-  className="absolute inset-0"
-  // frameBorder="0"
-  // marginHeight="0"
-  // marginWidth="0"
-  // scrolling="no"
-  src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-  style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.16)' }}
-></iframe>
+    <section className="text-gray-400 bg-gray-800 body-font relative">
+      <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-800 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            src="https://maps.google.com/maps?q=patna+&t=&z=13&ie=UTF8&iwloc=&output=embed"
+            style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.16)' }}
+          ></iframe>
 
-      <div className="bg-gray-800 relative flex flex-wrap py-6 rounded shadow-md">
-        <div className="lg:w-1/2 px-6">
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs">ADDRESS</h2>
-         <p className="mt-1">{`${contact.address.city}, ${contact.address.state} ${contact.address.zip}`}</p>
+          <div className="bg-gray-800 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">ADDRESS</h2>
+              <p className="mt-1">{`${contact.address.city}, ${contact.address.state} ${contact.address.zip}`}</p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">EMAIL</h2>
+              <a className="text-indigo-400 leading-relaxed">{contact.email}</a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">PHONE</h2>
+              <p className="leading-relaxed">{contact.phone}</p>
+            </div>
+          </div>
         </div>
-        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs">EMAIL</h2>
-          <a className="text-indigo-400 leading-relaxed">{contact.email}</a>
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">PHONE</h2>
-          <p className="leading-relaxed">{contact.phone}</p>
+        <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white text-lg mb-1 font-medium title-font">Write to me</h2>
+          <p className="leading-relaxed mb-5">{contact.contactmessage}</p>
+          <form onSubmit={handleSubmit}>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-400">Name</label>
+              <input type="text" id="name" name="name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} className="w-full bg-gray-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-400">Email</label>
+              <input type="email" id="email" name="email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} className="w-full bg-gray-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="message" className="leading-7 text-sm text-gray-400">Message</label>
+              <textarea id="message" name="message" value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })} className="w-full bg-gray-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            </div>
+            <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+            {submitSuccess && <p className="text-green-500 mt-2">Form submitted successfully!</p>}
+          </form>
         </div>
       </div>
-    </div>
-    <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-      <h2 className="text-white text-lg mb-1 font-medium title-font">Write to me</h2>
-      <p className="leading-relaxed mb-5">{contact.contactmessage}</p>
-      <div className="relative mb-4">
-        <label htmlFor="name" className="leading-7 text-sm text-gray-400">Name</label>
-        <input type="text" id="name" name="name" className="w-full bg-gray-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-400">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-gray-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-sm text-gray-400">Message</label>
-        <textarea id="message" name="message" className="w-full bg-gray-700 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-      <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      {/* <p className="text-xs text-gray-400 text-opacity-90 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p> */}
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
